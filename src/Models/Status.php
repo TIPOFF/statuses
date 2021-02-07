06ec30bdfbb05f7c87e45093baf403443559dedf
Drew Roberts <github@drewroberts.com>
2021-02-07 14:37:15 -0500

    Extend base model from tipoff/support

--- src/Models/Status.php
+++ src/Models/Status.php
@@ -2,10 +2,10 @@
 
 namespace Tipoff\Statuses\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Tipoff\Support\Models\BaseModel;
 use Tipoff\Support\Traits\HasPackageFactory;
 
-class Status extends Model
+class Status extends BaseModel
 {
     use HasPackageFactory;
 
