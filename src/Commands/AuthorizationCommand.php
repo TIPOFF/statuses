af6a0b68f9ac70227cae13b943b91966b146561d
Hunter Montell <huntermontell@protonmail.com>
2021-02-14 14:41:11 -0600

    Add strict types

--- src/Commands/AuthorizationCommand.php
+++ src/Commands/AuthorizationCommand.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tipoff\Statuses\Commands;
 
 use Illuminate\Console\Command;
--- src/Models/Status.php
+++ src/Models/Status.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tipoff\Statuses\Models;
 
 use Tipoff\Support\Models\BaseModel;
