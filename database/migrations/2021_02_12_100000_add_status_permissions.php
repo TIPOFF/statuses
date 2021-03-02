2a15fea9aed6ddee840c2a6c8384bbca6852209a
devjk1 <jinsik@omniadigital.io>
2021-03-02 17:47:00 -0500

    use new BasePermissionsMigration

--- database/migrations/2021_02_12_100000_add_status_permissions.php
+++ database/migrations/2021_02_12_100000_add_status_permissions.php
@@ -2,24 +2,18 @@
 
 declare(strict_types=1);
 
-use Illuminate\Database\Migrations\Migration;
-use Spatie\Permission\Contracts\Permission;
-use Spatie\Permission\PermissionRegistrar;
+use Tipoff\Authorization\Permissions\BasePermissionsMigration;
 
-class AddStatusPermissions extends Migration
+class AddStatusPermissions extends BasePermissionsMigration
 {
     public function up()
     {
-        if (app()->has(Permission::class)) {
-            app(PermissionRegistrar::class)->forgetCachedPermissions();
+        $permissions = [
+            'view statuses',
+            'create statuses',
+            'update statuses'
+        ];
 
-            foreach ([
-                         'view statuses',
-                         'create statuses',
-                         'update statuses'
-                     ] as $name) {
-                app(Permission::class)::findOrCreate($name, null);
-            };
-        }
+        $this->createPermissions($permissions);
     }
 }
