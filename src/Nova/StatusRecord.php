0de6c931155f04eed79db962c18bfa19cba80834
arwaawan <awan.arwa@aerocareusa.com>
2021-05-06 22:41:19 -0500

    fix nova resources

--- src/Nova/StatusRecord.php
+++ src/Nova/StatusRecord.php
@@ -7,6 +7,7 @@ namespace Tipoff\Statuses\Nova;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\MorphTo;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Panel;
@@ -28,18 +29,24 @@ class StatusRecord extends BaseResource
     {
         return array_filter([
             ID::make()->sortable(),
-            Text::make('Type', )->sortable(),
+            Text::make('Type')->sortable(),
             Text::make('Status', 'status.id', function () {
                 return $this->status->name;
             })->sortable(),
+            MorphTo::make('Statusable'),
         ]);
     }
 
     public function fields(Request $request)
     {
         return array_filter([
-            Text::make('Type'),
-            nova('status') ? BelongsTo::make('Status', 'status', nova('status'))->searchable() : null,
+            Text::make('Type')->rules(['required']),
+            nova('status') ? BelongsTo::make('Status', 'status', nova('status'))->searchable()->rules(['required']) : null,
+            MorphTo::make('Statusable')->types([
+                nova('booking'),
+                nova('contact'),
+                nova('order'),
+            ])->rules(['required']),
             new Panel('Data Fields', $this->dataFields()),
         ]);
     }
